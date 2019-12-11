f0ed6c2d714c8c5ec725ad4612793c2ddf54d21d
ggirlk <ggirlk2@gmail.com>
2019-12-11 21:15:11 +0000

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,16 +2,19 @@
 def uppercase(str):
     i = 0
     k = 0
+    _end = ""
     while i != len(str):
         a = 97
         b = 65
         k = 0
+        if i == len(str) - 1 :
+            _end = "\n"
         while a != 123:
             if ord(str[i]) == a or ord(str[i]) == b:
                 k = 1
-                print("{:c}".format(b), end="")
+                print("{:c}".format(b), end=_end)
             a += 1
             b += 1
         if k == 0:
-            print("{}".format(str[i]), end="")
+            print("{}".format(str[i]), end=_end)
         i += 1
