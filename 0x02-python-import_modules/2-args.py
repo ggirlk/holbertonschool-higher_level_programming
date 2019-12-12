1c09ca2cac304d244b8cb496122aec4958a40a81
ggirlk <ggirlk2@gmail.com>
2019-12-12 19:21:49 +0000

    1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
-i = 0
-while i != len(sys.argv):
-    print(sys.argv[i])
-    i += 1
+if __name__ == "__main__":
+    i = 0
+    while i != len(sys.argv):
+        print(sys.argv[i])
+        i += 1
