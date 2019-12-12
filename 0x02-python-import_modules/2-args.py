5ac82d7de3b463d3fc7e6f4d8dcacfa75b3db1e2
ggirlk <ggirlk2@gmail.com>
2019-12-12 19:46:57 +0000

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    i = 0
+    i = 1
+    print("{:d} arguments:".format(len(sys.argv)))
     while i != len(sys.argv):
         print("{:d}: {}".format(i, sys.argv[i]))
         i += 1
