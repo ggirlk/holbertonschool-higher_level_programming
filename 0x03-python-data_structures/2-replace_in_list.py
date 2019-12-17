7e161252285e8c8ba37f5d9feb71b6f91ce8b5f7
ggirlk <ggirlk2@gmail.com>
2019-12-17 04:04:41 +0000

    2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,4 @@
 def replace_in_list(my_list, idx, element):
-    for i in range(0, len(my_list)):
-        if (i == idx):
-            my_list[i] = element;
+        if (len(my_list) >= idx):
+            my_list[idx] = element;
+        return (my_list)
