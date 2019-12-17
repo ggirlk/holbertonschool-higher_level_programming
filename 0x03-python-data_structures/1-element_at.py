71c04c11de64da092dc3fe214d2c4948f7918db1
ggirlk <ggirlk2@gmail.com>
2019-12-17 03:52:34 +0000

    1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,4 @@
+def element_at(my_list, idx):
+    for i in range(0, len(my_list)):
+        if i == idx:
+            return(my_list[i])
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,4 @@
+def replace_in_list(my_list, idx, element):
+    for i in range(0, len(my_list)):
+        if (i == idx):
+            my_list[i] = element;
