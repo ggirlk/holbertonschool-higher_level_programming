9786013e8827212c54949b10f1fb0afc59da3591
ggirlk <ggirlk2@gmail.com>
2019-12-17 19:49:48 +0000

    0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in range(0, len(my_list)):
-        if (my_list[i] in range(0, 1000000)):
+        if (isinteger(my_list[i])):
             print("{:d}".format(my_list[i]))
+def isinteger(a):
+    try:
+        int(a)
+        return True
+    except ValueError:
+        return False
