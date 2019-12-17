606f28c735d026f97935940d7f6fb8ab060c58c5
ggirlk <ggirlk2@gmail.com>
2019-12-17 21:14:02 +0000

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    nstr = [""] * len(my_string)
+    nstr = ""
     for i in range(0, len(my_string)):
         if (my_string[i] != "c" and my_string[i] != "C"):
-            nstr[i] = my_string[i]
-    return "".join(nstr)
+            nstr += my_string[i]
+    return nstr
