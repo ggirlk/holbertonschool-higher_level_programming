c33117898524ce42f12cc6c56a9c8c75db7d6998
ggirlk <ggirlk2@gmail.com>
2019-12-18 18:36:16 +0000

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         a = ""
         for j in range(0, len(matrix[i])):
             if matrix[i][j] in range(0, 1000000):
-                a += format(matrix[i][j])
+                print("{:d}".format(matrix[i][j]), end="")
                 if j < len(matrix[i]) - 1:
-                    a += " "
-        print("{}".format(a))
+                     print(" ", end="")
+        print()
