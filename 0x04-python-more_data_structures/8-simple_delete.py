ea9a4b87fba5ccb2af49888a534374762d5c982a
ggirlk <ggirlk2@gmail.com>
2019-12-19 22:03:56 +0000

    78

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for i in sorted(a_dictionary):
-        if str(i) == key:
-            del a_dictionary[key]
+    if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary
