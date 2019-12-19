99220f612a574350671a39d46adebdad782baedb
ggirlk <ggirlk2@gmail.com>
2019-12-19 21:59:06 +0000

    78

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    s = list(a_dictionary)
-    for i in sorted(s):
-        if i == key:
-            setattr(a_dictionary, i, value)
+    a_dictionary[key] = value
     return a_dictionary
