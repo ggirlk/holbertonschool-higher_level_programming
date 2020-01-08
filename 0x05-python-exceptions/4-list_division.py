137a1ddf6c98be3c25b7e665906ead54f37ef152
ggirlk <ggirlk2@gmail.com>
2020-01-08 20:37:19 +0000

    Safe list printing

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,8 +13,8 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
             r = 0
         except ZeroDivisionError:
-            print("division by 0");
+            print("division by 0")
             r = 0
         finally:
-            nl[i] = r                
+            nl[i] = r
     return (nl)
