d1b08c5eda098325a127d782e3e09a32fab3bc78
ggirlk <ggirlk2@gmail.com>
2020-01-08 22:07:16 +0000

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     while i < x:
         try:
             my_list[i]
-        except IndexError:
+        except:
             break
         else:
             k += 1
