954826685e36f4dff4012c533b1eb1307f137193
ggirlk <ggirlk2@gmail.com>
2020-01-08 16:00:31 +0000

    Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,11 +3,11 @@ def safe_print_division(a, b):
     try:
         int(a) & int(b)
     except ValueError:
-        print("Inside result: None") 
+        print("Inside result: None")
         return (None)
     finally:
         if (b != 0):
-            r = a / b
+            r = a/b
             print("Inside result: {}".format(r))
             return (r)
         else:
