3c149df0df5553a5f52dcc947a581d74c2880a58
ggirlk <ggirlk2@gmail.com>
2020-01-13 23:45:48 +0000

    Integers addition

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 """ This is the "2-matrix_divided" module.
 The 2-matrix_divided module supplies one function, matrix_divided() For example;
->>> matrix_divided
-3
+>>> matrix = [[1, 2, 3],[4, 5, 6]]
+>>> matrix_divided(matrix, 3)
+[[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
 """
+
+
 def matrix_divided(matrix, div):
+    """ adding 2 integers
+        matrix: list of integers or floats
+        div: number
+    """
+    nw = []
+    i = 0
+    for mx in matrix:
+        
+        for my in mx:
+            if not (isinstance(my, float) or isinstance(my, int)):
+                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            
