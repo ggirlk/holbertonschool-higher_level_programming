bafb4ad3bdd41c7a252064235c79910b2557ca9a
ggirlk <ggirlk2@gmail.com>
2020-01-14 22:05:19 +0000

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,11 +12,20 @@ def matrix_divided(matrix, div):
         matrix: list of integers or floats
         div: number
     """
-    nw = []
-    i = 0
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    l0 = []
+    i = len(matrix[0])
     for mx in matrix:
-        
+        l1 = []
+        if len(mx) != i:
+            raise TypeError('Each row of the matrix must have the same size')        
         for my in mx:
             if not (isinstance(my, float) or isinstance(my, int)):
                 raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-            
+            r = my/div
+            r = int(r * 100)
+            r = r / 100
+            l1.append(r)
+        l0.append(l1)
+    return (l0)
