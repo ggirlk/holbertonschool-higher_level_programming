3a749918315aef04f0f6ff2f3fba3b796076392e
ggirlk <ggirlk2@gmail.com>
2020-01-14 22:28:45 +0000

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ This is the "2-matrix_divided" module.
-The 2-matrix_divided module supplies one function, matrix_divided() For example;
+The 2-matrix_divided module supplies one function, matrix_divided();
+For example;
 >>> matrix = [[1, 2, 3],[4, 5, 6]]
 >>> matrix_divided(matrix, 3)
 [[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
@@ -19,10 +20,12 @@ def matrix_divided(matrix, div):
     for mx in matrix:
         l1 = []
         if len(mx) != i:
-            raise TypeError('Each row of the matrix must have the same size')        
+            raise TypeError('Each row of the matrix must have the same size')
         for my in mx:
             if not (isinstance(my, float) or isinstance(my, int)):
-                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+                raise TypeError(
+                    'matrix must be a matrix (list of lists)of integers/floats'
+                )
             r = my/div
             r = int(r * 100)
             r = r / 100
