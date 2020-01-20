dca13bec82f954f4e5cdc1505cfe1886e768f46d
ggirlk <ggirlk2@gmail.com>
2020-01-20 12:56:31 +0000

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,12 +3,17 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     s = ""
+    k = 0
     for i in text:
         if s == "" and i == " ":
             pass
         else:
             s += i
         if (i == ".") or (i == "?"):
+            k = 1
             print(s)
             print()
             s = ""
+    if k == 0:
+        print(text)
+        print()
