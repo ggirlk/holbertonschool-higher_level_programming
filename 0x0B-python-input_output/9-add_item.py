ff2cf2fdd0f62d1a476455ab7956861cb436d58f
ggirlk <ggirlk2@gmail.com>
2020-01-23 21:07:03 +0000

    Load, add, save

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+save = __import__('7-save_to_json_file').save_to_json_file
+load = __import__('8-load_from_json_file').load_from_json_file
+
+
+def listappend(args, l=[]):
+    for i in range(1, len(args)):
+        l.append(args[i])
+    return l
+
+
+if __name__ == "__main__":
+    file = "add_item.json"
+    args = load(file) + listappend(sys.argv)
+    save(args, file)
+    load(file)
