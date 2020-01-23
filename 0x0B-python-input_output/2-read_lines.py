5f31f160dc35194b682d4244712160dca9625261
ggirlk <ggirlk2@gmail.com>
2020-01-23 09:16:22 +0000

    Read n lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+def number_of_lines(filename=""):
+    with open(filename, encoding='utf-8') as f:
+        i = 0
+        while f.readline():
+            i += 1
+        f.close()
+    return i
+
+
 def read_lines(filename="", nb_lines=0):
     with open(filename, encoding='utf-8') as f:
         i = 0
-        if nb_lines <= 0:
+        nb = number_of_lines(filename)
+        if nb_lines <= 0 or nb <= nb_lines:
             print(f.read())
         else:
             while i < nb_lines:
                 print(f.readline(), end="")
-                i+= 1
+                i += 1
+        f.close()
