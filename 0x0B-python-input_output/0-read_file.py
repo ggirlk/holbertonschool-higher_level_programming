b8dd44b134a6818e65bc0d391c8db255d2dd7145
ggirlk <ggirlk2@gmail.com>
2020-01-23 09:04:26 +0000

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     with open(filename, encoding='utf-8') as f:
-        stdout = open(1, 'w')
-        print(f.read(), file=stdout)
-        f.close()
+        print(f.read())
