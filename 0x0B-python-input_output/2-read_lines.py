6d583f56fe49bb014c05f12bdbc81bcb6c7d62e5
ggirlk <ggirlk2@gmail.com>
2020-01-23 09:08:37 +0000

    Read n lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,8 +3,8 @@ def read_lines(filename="", nb_lines=0):
     with open(filename, encoding='utf-8') as f:
         i = 0
         if nb_lines <= 0:
-            print(f.read(), flush=True)
+            print(f.read())
         else:
-            while i <= nb_lines:
-                print(f.readline(), end="", flush=True)
+            while i < nb_lines:
+                print(f.readline(), end="")
                 i+= 1
