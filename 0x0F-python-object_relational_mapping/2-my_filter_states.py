1b543009d78c13960cc770d03e7b80c7873d3162
ggirlk <ggirlk2@gmail.com>
2020-03-29 20:11:49 +0000

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Get all states"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    query = "SELECT id, name FROM states WHERE name = '{}'".format(argv[4])
+    query += "ORDER BY id ASC"
+    cur.execute(query)
+    req = cur.fetchall()
+    for r in req:
+        print("({}, '{}')".format(r[0], r[1]))
+    cur.close()
+    db.close()
