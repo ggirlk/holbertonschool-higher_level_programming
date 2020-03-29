8e0a26b10bd9b6c1d7ea256377c38a8305a30eaa
ggirlk <ggirlk2@gmail.com>
2020-03-29 19:49:09 +0000

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+"""Get all states"""
 
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    req = cur.fetchall()
+    for r in req:
+        print("({}, '{}')".format(r[0], r[1]))
+    cur.close()
+    db.close()
