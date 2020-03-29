8d0a3441b2b90b04d7a9210d4d5e0093f08e9da7
ggirlk <ggirlk2@gmail.com>
2020-03-29 19:02:32 +0000

    Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+
+db=MySQLdb._mysql.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+db.query("SELECT * FROM states ORDER BY id ASC")
+req=db.store_result()
+for r in req.fetch_row(5):
+    print(r)
