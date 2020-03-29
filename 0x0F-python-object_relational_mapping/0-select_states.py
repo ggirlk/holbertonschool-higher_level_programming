119f4b977dcaca2f4a955090046107ccf39dc6f0
ggirlk <ggirlk2@gmail.com>
2020-03-29 19:24:09 +0000

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """Get all states"""
 
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
 
-import sys
-import MySQLdb
 
-
-db = MySQLdb._mysql.connect(host="localhost",
+    db = MySQLdb._mysql.connect(host="localhost",
                             port=3306, user=sys.argv[1],
                             passwd=sys.argv[2], db=sys.argv[3])
-db.query("SELECT id, name FROM states ORDER BY id ASC")
-req = db.store_result()
-for r in req.fetch_row(maxrows=0):
-    print("({}, {})".format(r[0], r[1]))
+    db.query("SELECT id, name FROM states ORDER BY id ASC")
+    req = db.store_result()
+    for r in req.fetch_row(maxrows=0):
+        print("({}, {})".format(r[0], r[1]))
