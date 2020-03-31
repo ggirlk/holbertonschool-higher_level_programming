59d53f21457cd90179678027c79dd215ac4d55a3
ggirlk <ggirlk2@gmail.com>
2020-03-31 06:27:36 +0000

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session.configure(bind=engine)
     session = Session()
-    for state in session.query(State, City).filter(State.id==City. \
-        state_id).order_by(City.id).all():
-        print("{}: {}".format(state.id, state.name))
+    for result in session.query(State, City).filter(State.id==City.state_id).order_by(City.id).all():
+        city = result.City
+        state = result.State
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
