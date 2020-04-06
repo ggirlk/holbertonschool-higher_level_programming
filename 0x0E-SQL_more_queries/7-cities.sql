a8bb3bf9b8c33dcd1ddcdd12c96f69a33a40c580
ggirlk <ggirlk2@gmail.com>
2020-04-06 10:20:48 +0000

    Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    state_id int NOT NULL FOREIGN KEY REFERENCES states(id),
-    name varchar(256) NOT NULL
+    state_id int NOT NULL,
+    name varchar(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
