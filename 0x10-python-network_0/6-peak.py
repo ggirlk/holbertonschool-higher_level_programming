52207e47a9848892f7a3989dcb85ae446479d888
ggirlk <ggirlk2@gmail.com>
2020-04-12 16:29:49 +0000

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,13 +2,12 @@
 """Find a peak"""
 
 
-if __name__ == "__main__":
-    def find_peak(list_of_integers):
-        if not list_of_integers:
-            return None
-        else:
-            max = list_of_integers[0]
-            for i in list_of_integers:
-                if max < i:
-                    max = i
-            return max
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+    else:
+        max = list_of_integers[0]
+        for i in list_of_integers:
+            if max < i:
+                max = i
+        return max
