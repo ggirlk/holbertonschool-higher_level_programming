8ad0d1e25a64ff1190d29f1fc89087e0ecd2e9b7
ggirlk <ggirlk2@gmail.com>
2020-04-15 22:13:58 +0000

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+
+
+req = urllib.request.Request('https://intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+   the_page = response.read()
+print("Body response:")
+print("\t- type: {}".format(type(the_page)))
+print("\t- content: {}".format(the_page))
+s = str(the_page).encode('utf-8')
+print("\t- utf8 content: {}".format(s))
