65cd07c732d68f46027e272d90019d0cbf167497
ggirlk <ggirlk2@gmail.com>
2020-04-16 22:02:20 +0000

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Model to fetch web page """
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    data = {"email":sys.argv[2]}
+    data = urllib.parse.urlencode(data)
+    data = data.encode("ascii")
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
