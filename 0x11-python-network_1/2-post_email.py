0094285eab0b8d426223dcfc5290e555a4f5e4b2
ggirlk <ggirlk2@gmail.com>
2020-04-16 22:03:29 +0000

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,7 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    data = {"email":sys.argv[2]}
+    data = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(data)
     data = data.encode("ascii")
     req = urllib.request.Request(sys.argv[1], data)
