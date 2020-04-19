b459d8682cf57e860b71e0469a80b07129f658ab
ggirlk <ggirlk2@gmail.com>
2020-04-19 23:40:14 +0000

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,4 +7,4 @@ import sys
 if __name__ == "__main__":
     data = {"email": sys.argv[2]}
     req = requests.post(sys.argv[1], data)
-    print("Your email is: {}".format(req.text['email']))
+    print("Your email is: {}".format(req.json()["email"]))
