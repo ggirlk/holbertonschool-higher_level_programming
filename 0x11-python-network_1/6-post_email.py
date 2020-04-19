7f38fb1a3252a5e04c5b173e7bc5b25fba0c11c3
ggirlk <ggirlk2@gmail.com>
2020-04-19 23:08:00 +0000

    POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Model to fetch web page """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    data = {"email": sys.argv[2]}
+    req = requests.post(sys.argv[1], data)
+    print("Your email is: {}".format(req.text['email']))
