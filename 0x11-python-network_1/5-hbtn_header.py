334e9a1e3a0722e7872fcc7540b0460d176f14d5
ggirlk <ggirlk2@gmail.com>
2020-04-19 22:54:21 +0000

    Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,4 +6,5 @@ import sys
 
 if __name__ == "__main__":
     req = requests.get(sys.argv[1])
-    print(req.headers["X-Request-Id"])
+    if "X-Request-Id" in req.headers:
+        print(req.headers["X-Request-Id"])
