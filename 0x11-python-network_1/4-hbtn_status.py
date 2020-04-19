28a8909e97df987b6c9255b2c2f9f710fa9fe89f
ggirlk <ggirlk2@gmail.com>
2020-04-19 22:33:51 +0000

    What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Model to fetch web page """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    req = requests.get("https://intranet.hbtn.io/status")
+    the_page = req.content
+    print("\t- type: {}".format(type(the_page)))
+    print("\t- content: {}".format(the_page.decode('utf-8')))
