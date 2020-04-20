3441d175af4f6f7ed3021a74f153648502b68164
ggirlk <ggirlk2@gmail.com>
2020-04-20 00:36:07 +0000

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,10 +5,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    try:
-        req = requests.get(sys.argv[1])
-        print(req.content.decode("utf-8"))
-    except requests.exceptions.HTTPError as ex:
-        e = ex.decode('utf-8')
-        e = ''.join(x for x in e if x.isdigit())
-        print("Error code: {}".format(e))
+    req = requests.get(sys.argv[1])
+    if req.status_code > 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.content.decode('utf-8'))
