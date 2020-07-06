37cf269e0d74883c45c103933814749f9ce165dc
ggirlk <ggirlk2@gmail.com>
2020-07-06 23:36:35 +0100

    values

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,13 +5,11 @@ let str = '';
 process.argv.forEach((val, index) => {
   if (i > 1) {
     if (`${val}`) {
-      str += `${val} `;
+      console.log(`${val}`);
     }
   }
   i++;
 });
 if (i === 2) {
   console.log('No argument');
-} else {
-  console.log(str);
 }
