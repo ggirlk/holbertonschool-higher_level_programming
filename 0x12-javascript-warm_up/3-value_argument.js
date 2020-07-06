64cc6494f7de2e32ceaffd0aac7ce12d6e554759
ggirlk <ggirlk2@gmail.com>
2020-07-06 22:31:53 +0100

    Value of my argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Arguments
+let i = 0;
+process.argv.forEach((val, index) => {
+  if (i > 1) {
+    if (`${val}`) {
+	console.log(`${val}`);
+    }
+  }
+  i++;
+});
+if (i == 2) {
+console.log(`No argument`);
+}
