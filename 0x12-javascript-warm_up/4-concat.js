85af8f2f89d2e2b06bdf0e041d7eac87c46ac03e
ggirlk <ggirlk2@gmail.com>
2020-07-07 01:02:52 +0100

    concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,17 @@
 #!/usr/bin/node
-let str = "";
 let args = process.argv.slice(2);
-console.log(args.join(" "))
+let str = '';
+if (args[0])
+{
+str += args[0];
+} else {
+str += "undefined";
+}
+str += " is ";
+if (args[1])
+{
+str += args[1];
+} else {
+str += "undefined";
+}
+console.log(str);
