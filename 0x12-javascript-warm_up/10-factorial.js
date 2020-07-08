535d448d02bf4d408c43f618dd1c867787bc8dc5
ggirlk <ggirlk2@gmail.com>
2020-07-08 04:16:13 +0100

    second biggest

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const x = Number(args[0]);
-function Factorial(n) {
-let k=1;
-for (let i = 2; i <= n; i++)
-k = k * i;
-return k;
+function Factorial (n) {
+  let k = 1;
+  for (let i = 2; i <= n; i++) { k = k * i; }
+  return k;
 }
 console.log(Factorial(x));
